3fd9cd0067a32666abdeb1e3e958a79052851367
algirdas-zyro <algirdas@zyro.com>
2021-03-12 21:00:58 +0200

    Working snappable POC

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,189 @@
 <template>
   <div class="page main">
-    <div class="container">
+    isResizingLeft: {{ isResizingLeft }} <br />
+    isResizingRight: {{ isResizingRight }}
+    <div class="grid" ref="grid" :style="gridStyle">
       <Moveable
         class="moveable"
         v-bind="moveable"
+        @drag-start="handleDragStart"
         @drag="handleDrag"
+        @drag-end="handleDragEnd"
+        @resize-start="handleResizeStart"
         @resize="handleResize"
-        @scale="handleScale"
-        @rotate="handleRotate"
-        @warp="handleWarp"
+        @resize-end="handleResizeEnd"
       >
-        <span>Vue Moveable</span>
+        <p>some relatively sized text</p>
       </Moveable>
-      <div class="buttons able">
-        <a
-          href="#"
-          v-for="(state, key) in states"
-          :key="key"
-          :class="{ selected: currentState === key }"
-          @click="currentState = key"
-        >{{ state }}</a>
-      </div>
-      <!-- <Badges/> -->
-      <p class="description">Vue Moveable is Draggable! Resizable! Scalable! Rotatable!</p>
-      <div class="buttons">
-        <a href="https://github.com/probil/vue-moveable" target="_blank">Download</a>
-        <a href="https://daybrush.com/moveable/release/latest/doc/" target="_blank">API</a>
+      <div class="tiles" v-if="isResizingLeft || isResizingRight">
+        <div class="tiles__row" v-for="row in rowCount" :key="`row${row}`">
+          <div
+            class="tiles__col"
+            v-for="column in columnCount"
+            :key="`column${column}`"
+          />
+        </div>
+        <div
+          v-if="isResizingLeft && isSnapEnabled"
+          class="tiles__guide"
+          :style="{ left: `${activeLeftGuide}px` }"
+        />
+        <div
+          v-if="isResizingRight && isSnapEnabled"
+          class="tiles__guide"
+          :style="{ left: `${activeRightGuide}px` }"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-/* eslint-disable no-param-reassign,no-unused-expressions,no-console */
 import Moveable from "vue-moveable";
-// import Badges from "./components/Badges.vue";
+
+const INITIAL_GRID_WIDTH = 1224;
+const INITIAL_GRID_HEIGHT = 80;
+const INITIAL_COLUMN_GAP = 24;
+const INITIAL_COLUMN_COUNT = 12;
+const INITIAL_ROW_HEIGHT = 80;
+const INITIAL_ROW_GAP = 24;
+const INITIAL_ROW_COUNT = 3;
 
 export default {
   name: "app",
   components: {
     Moveable,
-    // Badges
   },
-  data: () => ({
-    moveable: {
-      draggable: true,
-      throttleDrag: 1,
-      resizable: false,
-      throttleResize: 1,
-      keepRatio: false,
-      scalable: true,
-      throttleScale: 0.01,
-      rotatable: true,
-      throttleRotate: 0.2,
-      pinchable: true,
-      origin: false
-    },
-    states: {
-      scalable: "Scalable",
-      resizable: "Resizable",
-      warpable: "Warpable"
-    },
-    currentState: "scalable"
-  }),
+  data() {
+    return {
+      moveable: {
+        passDragArea: true,
+        draggable: true,
+        throttleDrag: 1,
+        resizable: true,
+        throttleResize: 1,
+        edges: true,
+        renderDirections: ["e", "w"],
+        container: this.$refs.grid,
+      },
+      isSnapEnabled: true,
+      gridWidth: INITIAL_GRID_WIDTH,
+      gridHeight: INITIAL_GRID_HEIGHT,
+
+      columnGap: INITIAL_COLUMN_GAP,
+      columnCount: INITIAL_COLUMN_COUNT,
+      rowHeight: INITIAL_ROW_HEIGHT,
+      rowGap: INITIAL_ROW_GAP,
+      rowCount: INITIAL_ROW_COUNT,
+
+      // temporary point to hold left value
+      componentLeftInitial: 0,
+      componentLeft: 0,
+      componentTop: 0,
+      componentWidth: 200,
+
+      isResizingLeft: false,
+      isResizingRight: false,
+    };
+  },
   methods: {
-    handleDrag({ target, transform }) {
-      console.log("onDrag", transform);
-      target.style.transform = transform;
+    handleDragStart(e) {
+      this.isResizingLeft = true;
+      this.isResizingRight = true;
     },
-    handleResize({ target, width, height }) {
-      console.log("onResize", width, height);
-      target.style.width = `${width}px`;
-      target.style.height = `${height}px`;
+    handleDrag(e) {
+      this.componentTop = e.top;
+      this.componentLeft = e.left;
+      e.target.style.top = `${e.top}px`;
+      e.target.style.left = `${e.left}px`;
     },
-    handleScale({ target, transform }) {
-      console.log("onScale", transform);
-      target.style.transform = transform;
+    handleDragEnd(e) {
+      this.isResizingLeft = false;
+      if (this.isSnapEnabled) {
+        this.componentLeft = this.activeLeftGuide;
+        this.componentWidth = this.activeRightGuide - this.activeLeftGuide;
+        e.target.style.left = `${this.componentLeft}px`;
+        e.target.style.width = `${this.componentWidth}px`;
+      }
+      this.isResizingLeft = false;
+      this.isResizingRight = false;
     },
-    handleRotate({ target, transform }) {
-      console.log("onRotate", transform);
-      target.style.transform = transform;
+    handleResizeStart(e) {
+      this.componentLeftInitial = this.componentLeft;
+      this.isResizingLeft = e.direction[0] === -1;
+      this.isResizingRight = e.direction[0] === 1;
     },
-    handleWarp({ target, transform }) {
-      console.log("onWarp", transform);
-      target.style.transform = transform;
+    handleResize(e) {
+      this.componentWidth = e.target.clientWidth;
+      e.target.style.width = `${e.width}px`;
+
+      // only on left handle
+      if (this.isResizingLeft) {
+        this.componentLeft = this.componentLeftInitial - e.dist[0];
+        e.target.style.left = `${this.componentLeft}px`;
+      }
     },
-    clearAllStates() {
-      Object.keys(this.states).forEach(key => {
-        this.moveable[key] = false;
-      });
-    }
+    handleResizeEnd(e) {
+      if (this.isSnapEnabled) {
+        this.componentLeft = this.activeLeftGuide;
+        this.componentWidth = this.activeRightGuide - this.activeLeftGuide;
+        e.target.style.left = `${this.componentLeft}px`;
+        e.target.style.width = `${this.componentWidth}px`;
+      }
+      this.isResizingLeft = false;
+      this.isResizingRight = false;
+    },
   },
-  watch: {
-    currentState(newState) {
-      this.clearAllStates();
-      this.moveable[newState] = true;
-    }
-  }
+  computed: {
+    gridStyle({
+      gridWidth,
+      gridHeight,
+      componentWidth,
+      componentLeft,
+      componentTop,
+    }) {
+      return {
+        "--grid-width": `${gridWidth}px`,
+        "--grid-height": `${gridHeight}px`,
+        "--component-width": `${componentWidth}px`,
+        "--component-left": `${componentLeft}px`,
+        "--component-top": `${componentTop}px`,
+      };
+    },
+    columnWidth: ({ gridWidth, columnCount, columnGap }) => {
+      return (gridWidth + columnGap) / columnCount - columnGap;
+    },
+    leftTileGuides: ({ columnCount, columnGap, columnWidth }) => {
+      return [...Array(columnCount)].map(
+        (column, i) => i * (columnWidth + columnGap)
+      );
+    },
+    activeLeftGuide: ({ leftTileGuides, componentLeft }) => {
+      return leftTileGuides.reduce((prev, curr) => {
+        return Math.abs(curr - componentLeft) < Math.abs(prev - componentLeft)
+          ? curr
+          : prev;
+      });
+    },
+    activeRightGuide: ({ rightTileGuides, componentLeft, componentWidth }) => {
+      const right = componentLeft + componentWidth;
+      return rightTileGuides.reduce((prev, curr) => {
+        return Math.abs(curr - right) < Math.abs(prev - right) ? curr : prev;
+      });
+    },
+    rightTileGuides: ({ leftTileGuides, columnWidth }) => {
+      return leftTileGuides.map((edge) => edge + columnWidth);
+    },
+  },
+  mounted() {
+    // don't forget tho throttle this!
+    const resizeObserver = new ResizeObserver(([{ contentRect }]) => {
+      this.gridWidth = contentRect.width;
+      this.gridHeight = contentRect.height;
+    });
+
+    resizeObserver.observe(this.$refs.grid);
+  },
 };
 </script>
 
@@ -114,87 +201,49 @@ body {
   font-weight: 300;
 }
 
-a {
-  text-decoration: none;
-  color: #333;
-}
-
-.page {
+.grid {
   position: relative;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-}
-
-.page:nth-child(2n) {
-  background: #f0f0f0;
-}
-.page.main {
-  z-index: 1;
-  min-height: 700px;
-}
-
-.container {
-  position: relative;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  /* max-width: var(--grid-width); */
+  /* max-width: 1224px; */
+  max-width: var(--grid-width);
+  margin: auto;
 }
 
 .moveable {
-  font-family: "Roboto", sans-serif;
-  position: relative;
-  width: 300px;
-  height: 200px;
-  text-align: center;
-  font-size: 40px;
-  margin: 0 auto;
-  font-weight: 100;
-  letter-spacing: 1px;
+  position: absolute;
+  width: var(--component-width);
+  left: var(--component-left);
+  top: var(--component-top);
 }
 
-.moveable span {
+.moveable .image {
+  width: 100%;
+  display: block;
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
-  white-space: nowrap;
+  transform: translate3d(-50%, -50%, 0);
 }
 
-.description {
-  text-align: center;
+.tiles__row {
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: repeat(12, 1fr);
+  margin-bottom: 12px;
 }
 
-.buttons.able {
-  margin-top: 16px;
-}
-.buttons.able a {
-  min-width: auto;
-  padding: 8px 20px;
-}
-.buttons {
-  text-align: center;
-  margin: 0;
-  padding: 10px;
+.tiles__col {
+  height: 50px;
+  background-color: rgba(0, 0, 0, 0.1);
+  border: solid 1px rgba(255, 255, 255, 0.35);
 }
 
-.buttons a {
-  position: relative;
-  text-decoration: none;
-  color: #333;
-  border: 1px solid #333;
-  padding: 12px 30px;
-  min-width: 140px;
-  text-align: center;
-  display: inline-block;
-  box-sizing: border-box;
-  margin: 5px;
-  transition: all ease 0.5s;
-}
-
-.buttons a:hover,
-.buttons a.selected {
-  background: #333;
-  color: #fff;
+.tiles__guide {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 1px;
+  background-color: #4af;
+  /* opacity: 0; */
 }
 </style>
